98b34c98b80cd40b47cb55521b3a9abeba7eee4f
gbwilliamson4 <90010953+gbwilliamson4@users.noreply.github.com>
2023-05-02 06:50:56 -0600

    first commit

---
+++ GeorgeTest.py
@@ -0,0 +1,18 @@
+import sys
+import pygame
+
+class MyWindow:
+    def __init__(self):
+        pygame.init()
+        
+
+    def run_Window(self):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    # Make an instance and run the game
+    ai = MyWindow()
+    ai.run_Window()
